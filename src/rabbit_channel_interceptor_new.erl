e72c9bd254a3d58dd8fba401c4c8aaff91618cf7
ash-lshift <ash@lshift.net>
2015-07-15 11:50:35 +0100

    wip

--- src/rabbit_channel_interceptor_new.erl
+++ src/rabbit_channel_interceptor_new.erl
@@ -32,6 +32,7 @@
 init(Ch) ->
   R = (catch begin
     Mods = lookup_all(?OLD_REGISTRY_CLASS) ++ lookup_all(?NEW_REGISTRY_CLASS),
+    check_no_overlap(Mods),
     {Hot, Cold} = lists:partition(fun ({_, M}) -> is_hot(M) end, Mods),
     {to_fn(Hot, Ch), to_fn(Cold, Ch)}
   end),
@@ -41,11 +42,22 @@ init(Ch) ->
 lookup_all(Class) ->
   [{Class, M} || {_, M} <- rabbit_registry:lookup_all(Class)].
 
+% Turn a list of interceptor modules into a single function
+
 to_fn(Mods, Ch) ->
   case Mods of
     [] -> fun(M, C) -> {M, C} end;
     [Mod] -> to_fn1(Mod, Ch)
+    [Mod|Mods1] ->
+      Fn1 = to_fn1(Mod, Ch),
+      Fn2 = to_fn(Mods1, Ch),
+      fun(M, C) ->
+          {M1, C1} = Fn1(M, C),
+          Fn2(M1, C1)
+      end
   end.
+
+% Turn a single interceptor module into a function
 
 to_fn1({channel_interceptor, Mod}, Ch) ->
   VHost = rabbit_channel:get_vhost(Ch),
@@ -54,6 +66,23 @@ to_fn1({channel_interceptor, Mod}, Ch) ->
 to_fn1({channel_interceptor_new, Mod}, Ch) ->
   St = Mod:init(Ch),
   fun (M, C) -> Mod:intercept(M, C, St) end.
+
+check_no_overlap(Mods) ->
+  check_no_overlap1([get_applicable_methods(Mod) || Mod <- Mods]).
+
+check_no_overlap1(Sets) ->
+  lists:foldl(fun(Set, Union) ->
+                  0 = gb_sets:size(gb_sets:intersection(Set, Union)),
+                  gb_sets:union(Set, Union)
+              end,
+              gb_sets:empty(),
+              Sets),
+  ok.
+
+get_applicable_methods({_, Mod}) ->
+  Methods = ['basic.publish', 'basic.ack', 'basic.nack', 'basic.reject',
+             'basic.credit'],
+  gb_sets:from_list([Method || Method <- Methods, Mod:applies_to(Method)]).
 
 is_hot(M) ->
   M:applies_to('basic.publish') orelse
